0805c414d7773edbedb67885521a1378e8037c6b
icarrera <irismcarrera@gmail.com>
2016-04-11 13:43:16 -0700

    Created a Photo class with attributes

--- imagersite/imager_images/models.py
+++ imagersite/imager_images/models.py
@@ -1,3 +1,30 @@
+# -*-coding: utf-8 -*-
+"""Imager image classes."""
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+PRIVACY_SETTINGS = [
+    ('private', 'private'),
+    ('shared', 'shared'),
+    ('public', 'public')
+]
+
+
+class Photo(models.Model):
+    """Individual picture uploaded by a user."""
+
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, null=False)
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    date_uploaded = models.DateField(auto_now_add=True)
+    date_modified = models.DateField(auto_now=True)
+    date_published = models.DateField(null=True, blank=True)
+    published = models.CharField(max_length=255,
+                                 choices=PRIVACY_SETTINGS,
+                                 default='private')
+
+
+class Album(models.Model):
+    """Photos and meta-data about the photos."""
+
+    pass
