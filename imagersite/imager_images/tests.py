5a4a42ce4c8ef9830a6ba84c642bb404906c2344
icarrera <irismcarrera@gmail.com>
2016-04-12 10:54:06 -0700

    added test placeholders

--- imagersite/imager_images/tests.py
+++ imagersite/imager_images/tests.py
@@ -2,6 +2,7 @@
 from __future__ import unicode_literals
 from django.test import TestCase
 from django.contrib.auth.models import User
+from imager_profile.tests import UserFactory
 import factory
 
 from imager_images.models import Photo, Album
@@ -18,8 +19,34 @@ class AlbumFactory(factory.django.DjangoModelFactory):
 
 class PhotoTestCase(TestCase):
     def setUp(self):
+        self.user = UserFactory.create(
+            username='jaimie',
+            email='jaimie@example.com'
+        )
+        self.user.set_password('stuff')
+
+        self.image = PhotoFactory.create(
+        title="image-1",
+        user=self.user,
+        description="This is a nature shot.",
+
+        )
+
+    def test_photo_has_user(self):
+        pass
+
+    def test_user_has_photo(self):
         pass
 
 class AlbumTestCase(TestCase):
-    def setup(self):
+    def setUp(self):
+        pass
+
+    def test_user_has_album(self):
+        pass
+
+    def test_photo_in_album(self):
+        pass
+
+    def test_photo_in_multiple_albums(self):
         pass
