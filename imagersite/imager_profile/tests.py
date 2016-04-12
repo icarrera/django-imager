fb65d6cf4cf638281a81a25d13c82b543352609c
icarrera <irismcarrera@gmail.com>
2016-04-11 19:09:14 -0700

    test if profile exists, add friends, delete user

--- imagersite/imager_profile/tests.py
+++ imagersite/imager_profile/tests.py
@@ -1,7 +1,11 @@
+"""Test ImagerProfile model."""
 from __future__ import unicode_literals
 from django.test import TestCase
 from django.contrib.auth.models import User
 import factory
+
+from imager_profile.models import ImagerProfile
+
 
 class UserFactory(factory.django.DjangoModelFactory):
     class Meta:
@@ -11,14 +15,38 @@ class UserFactory(factory.django.DjangoModelFactory):
 class UserTestCase(TestCase):
 
     def setUp(self):
-        self.user = UserFactory.create(
+        self.user_1 = UserFactory.create(
             username='bob',
-            email='bob@blahblah.com',
+            email='bob@example.com',
         )
-        self.user.set_password("secret")
+        self.user_1.set_password('secret')
 
-    def test_foo(self):
-        pass
+        self.user_2 = UserFactory.create(
+            username='sally',
+            email='sally@example.com',
+        )
+        self.user_2.set_password('moresecret')
 
-        # never create a user and just put the password in
+    def test_user_has_profile(self):
+        self.assertTrue(self.user_1.profile)
+
+    def test_how_many_profiles(self):
+        self.assertEqual(len(ImagerProfile.objects.all()), 2)
+
+    def test_expected_user(self):
+        self.assertEqual(self.user_1.username, ImagerProfile.active.all()[0].user.username)
+
+    def test_expected_email(self):
+        self.assertEqual(self.user_2.email, ImagerProfile.active.all()[1].user.email)
+
+    def test_add_friends(self):
+        bob_pr = self.user_1.profile
+        bob_pr.friends.add(self.user_2)
+        self.assertEqual(bob_pr.friends.all()[0], self.user_2)
+        self.assertEqual(self.user_2.friend_of.all()[0], self.user_1.profile)
+
+    def test_delete_user(self):
+        ImagerProfile.active.all()[0].delete()
+        self.assertNotIn(self.user_1.profile, ImagerProfile.active.all())
+
         # use the set password method to hash your password
