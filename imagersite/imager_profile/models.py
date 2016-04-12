2db2d52ea197a795190ce128e8e7f6b4c449c0df
icarrera <irismcarrera@gmail.com>
2016-04-12 00:08:34 -0700

    renamed photo categories

--- imagersite/imager_profile/models.py
+++ imagersite/imager_profile/models.py
@@ -1,6 +1,27 @@
 """Imager Profile model classes."""
+from __future__ import unicode_literals
 from django.db import models
 from django.conf import settings
+from django.utils.encoding import python_2_unicode_compatible
+
+PHOTO_CATEGORY = [
+    ('portrait', 'Portrait'),
+    ('nature', 'Nature'),
+    ('sports', 'Sports'),
+]
+
+US_REGIONS = [
+    ('pnw', 'Pacific Northwest'),
+    ('ne', 'New England'),
+    ('ma', 'Mid-Atlantic'),
+    ('se', 'Southeast'),
+    ('mw', 'Midwest'),
+    ('ds', 'Deep South'),
+    ('sw', 'Southwest'),
+    ('cf', 'California'),
+    ('ak', 'Alaska'),
+    ('hi', 'Hawaii')
+]
 
 
 class ActiveUserManager(models.Manager):
@@ -9,9 +30,10 @@ class ActiveUserManager(models.Manager):
     def get_queryset(self):
         """Return query set of profiles with active users."""
         queryset = super(ActiveUserManager, self).get_queryset()
-        return queryset.filter(user__is_active__=True)
+        return queryset.filter(user__is_active=True)
 
 
+@python_2_unicode_compatible
 class ImagerProfile(models.Model):
     """Profile attached to user model."""
 
@@ -20,11 +42,14 @@ class ImagerProfile(models.Model):
                                 on_delete=models.CASCADE,
                                 )
     camera_model = models.CharField(max_length=255)
-    location = models.CharField(max_length=255)
-    photography_type = models.CharField(max_length=255)
+    location = models.CharField(max_length=3,
+                                choices=US_REGIONS)
+    photography_type = models.CharField(max_length=255,
+                                        choices=PHOTO_CATEGORY)
     friends = models.ManyToManyField(settings.AUTH_USER_MODEL,
                                      related_name='friend_of')
     active = ActiveUserManager()
+    objects = models.Manager()
 
     def __str__(self):
         """Return string output of username."""
