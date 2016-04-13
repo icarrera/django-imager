31d12690cb38aab334d221cd6ea4aa225345f477
icarrera <irismcarrera@gmail.com>
2016-04-12 18:35:26 -0700

    uncommented code..whoops

--- imagersite/imager_images/apps.py
+++ imagersite/imager_images/apps.py
@@ -4,6 +4,6 @@ from django.apps import AppConfig
 class ImagerImagesConfig(AppConfig):
     name = 'imager_images'
 
-    # def ready(self):
-    #     """code to run when app is ready."""
-    #     from imager_images import handlers
+    def ready(self):
+        """code to run when app is ready."""
+        from imager_images import handlers
--- imagersite/imager_profile/apps.py
+++ imagersite/imager_profile/apps.py
@@ -4,6 +4,6 @@ from django.apps import AppConfig
 class ImagerProfileConfig(AppConfig):
     name = 'imager_profile'
 
-    # def ready(self):
-    #     """Code to run when the app is ready."""
-    #     from imager_profile import handlers
+    def ready(self):
+        """Code to run when the app is ready."""
+        from imager_profile import handlers
